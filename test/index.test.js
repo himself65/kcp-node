cddc8b190c919a0fb1747499015e80d53363c027
himself65 <himself65@outlook.com>
2019-06-18 20:08:35 +0800

    test: function 'create' type check test

--- test/index.test.js
+++ test/index.test.js
@@ -5,6 +5,15 @@ describe('kcp-node base unit test', () => {
 		expect(typeof kcp_node.create).toBe('function')
 	})
 
+	it('should throw error when no parameter', () => {
+		expect(() => kcp_node.create()).toThrowError()
+		expect(() => kcp_node.create(null)).toThrowError()
+		expect(() => kcp_node.create(undefined)).toThrowError()
+		expect(() => kcp_node.create({})).toThrowError()
+		expect(() => kcp_node.create([])).toThrowError()
+		expect(() => kcp_node.create("")).toThrowError()
+	})
+
 	it('should function create returns an object', () => {
 		expect(typeof kcp_node.create(3000)).toBe('object')
 	})
